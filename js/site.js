c719cdb9d4da50300db472df98cd707a6adeb1b7
Cyriel <jlppcprog@ntymail.com>
2019-05-01 14:36:25 +0200

    Update site.js

--- js/site.js
+++ js/site.js
@@ -10,8 +10,8 @@ function linuxDown(){
     goTo("https://github.com/OpMonTeam/OpMon/releases/download/alpha-v" + version + "/OpMon-" + version  + "-Linux.deb");
 }
 function macDown(){
-	alert("Warning : 0.15.1 is not released on MacOS yet. You can download the 0.14 by clicking on \"Other versions\"\nAttention : La 0.15 n'est pas encore sortie sur MacOS. Vous pouvez télécharger la 0.14 en cliquant sur \"Autres versions\"");
-    //goTo("https://github.com/OpMonTeam/OpMon/releases/download/alpha-v" + version + "/OpMon-Mac_" + version + ".dmg");
+	alert("Warning : You are now downloading a development version, since it's the only version currently avilable on mac. It's only temporary, but there can be many bugs.\nAttention : Vous vous apprêtez à télécharger une version de développement, car c'est la seule version actuellement disponible pour mac. C'est seulement temporaire, mais il peut y avoir beaucoup de bugs.");
+    goTo("https://github.com/OpMonTeam/OpMon/releases/download/alpha-v" + version + "/OpMon-Mac.dmg");
 }
 function linuxDownScript(){
     goTo("https://github.com/OpMonTeam/OpMon/releases/download/alpha-v" + version + "/source.sh");
